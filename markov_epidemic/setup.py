6edc9efa5f453e52348eacb70d05c52c273c4a29
sauxpa <patrick.jr.saux@gmail.com>
2020-04-09 17:20:27 +0200

    ready to ship

--- markov_epidemic/setup.py
+++ markov_epidemic/setup.py
@@ -1,6 +1,17 @@
 from setuptools import setup
 
-setup(name='markov_epidemic',
-      version='0.0.1',
-      install_requires=['numpy', 'networkx']
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='markov_epidemic',
+    version='1.0.1',
+    author="Patrick Saux",
+    author_email="patrick.jr.saux@gmail.com",
+    description="Library for stochastic simulation and study of epidemics.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sauxpa/markov_epidemic",
+    install_requires=['numpy', 'networkx'],
+    python_requires='>=3.6',
 )
